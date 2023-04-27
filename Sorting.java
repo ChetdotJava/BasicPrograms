eb06834d338348034c9bdd6f77260f0c5b758cce
ChetdotJava <122887498+ChetdotJava@users.noreply.github.com>
2023-04-27 19:39:42 +0530

    Add files via upload
    This program sorts the given array in ascending order using Selection Sort Algorithm.

---
+++ Sorting.java
@@ -0,0 +1,25 @@
+public class Sorting {
+    public static void selectionSort(int a[]){
+        for(int i=0; i<a.length-1; i++){
+            int small = i;
+            for(int j=i+1; j<a.length; j++){
+                if(a[j] < a[small]){
+                    small = j;}
+            }
+            int temp = a[i];
+            a[i] = a[small]; //puts small in i th element
+            a[small] = temp;
+        }
+    }
+    
+    public static void printArray(int[] a){
+        for(int i : a){
+            System.out.print(i+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int a[] ={8,4,1,3};
+        selectionSort(a);
+        printArray(a);
+        }
+}
