4110b4bf5b3e0c1c0054043514c3a032d541ba4a
ChetdotJava <122887498+ChetdotJava@users.noreply.github.com>
2023-06-28 19:32:28 +0530

    HollowPyramidPattern
    This program prints hollow pyramid pattern with last row printed completely.

---
+++ PatternPrinting.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PatternPrinting {
+    static void printHollowPyramid(int row){
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=(row-i); j++){
+                System.out.print(" ");
+            }if(i < row){
+                for(int j=1; j<=((2*i)-1); j++){
+                    if(j==1 || j==((2*i)-1)){
+                        System.out.print(i);
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }else{
+                for(int j=1; j<=((2*i)-1); j++){
+                    System.out.print(i);
+                }
+            }System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the value of row: ");
+        int row = inp.nextInt();
+        printHollowPyramid(row);
+        inp.close();
+    }
+}
